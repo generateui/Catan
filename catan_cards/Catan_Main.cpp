e2a9961245b99a68a797cec75ed43631ca01c47e
Suneil Mistry <smistry@vt.edu>
2012-04-10 11:21:23 -0400

    organized things into folders

---
+++ catan_cards/Catan_Main.cpp
@@ -0,0 +1,18 @@
+#include "Player.h"
+
+int main(int argc, char *argv[]){
+    
+    Player player1;
+    player1.checkStats();
+    player1.seeResources();
+    player1.seeDevelopments();
+    player1.buyItem();
+    player1.checkStats();
+    
+
+    /*
+    int a = roll(); //i'm doing something dumb with this function call that is a simple fix
+    int b = roll();
+    cout<<"a: "<<a<<" b: "<<b<<" total: "<<a+b<<endl;
+    */
+}
